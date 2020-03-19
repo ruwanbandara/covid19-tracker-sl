7f65120e10f568adc766af582cab087632cc8918
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-20 00:52:06 +0530

    updated for heroku deploy

--- src/app/service/covid19.service.ts
+++ src/app/service/covid19.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Covid19Service {
   // tslint:disable-next-line:variable-name
-  private _dataUrl = 'http://hpb.health.gov.lk/api/get-current-statistical';
+  private _dataUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
-  getData() {
+  getData(): Observable<any> {
     return this.http.get<any>(this._dataUrl);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://hpb.health.gov.lk/api/get-current-statistical'
 };
